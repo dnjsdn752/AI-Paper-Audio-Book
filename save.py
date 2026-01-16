7db92f312c0d5cd2ddcb25770c0094b9b4ab8255
dnjsdn752 <dnjsdn752@naver.com>
2026-01-16 18:39:08 +0900

    fix: 파일 명 변경 및 위치 이동

--- save.py
+++
@@ -1,7 +0,0 @@
-from transformers import AutoProcessor, AutoModelForCausalLM
-'''
-processor = AutoProcessor.from_pretrained("microsoft/git-base")
-model = AutoModelForCausalLM.from_pretrained("microsoft/git-base")
-
-processor.save_pretrained("./model/microsoft_git_base")
-model.save_pretrained("./model/microsoft_git_base")
